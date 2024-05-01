6f5107beb981499e3633b849ed6300676b9dcb02
Matti Räty <mattiraty@hotmail.com>
2024-05-01 15:44:10 +0300

    refactored to render Question component instead of QuestionTimer and Answers

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,39 +1,20 @@
 import React, { useCallback, useState } from 'react'
 import QuizOverImg from '../assets/quiz-complete.png'
 import QUESTIONS from '../questions'
-import QuestionTimer from './QuestionTimer';
-import Answers from './Answers';
-
-export enum AnswerState {
-    Correct = 'correct',
-    Wrong = 'wrong',
-    Answered = 'answered',
-    Unanswered = 'unanswered'
-}
+import Question from './Question';
 
 export default function Quiz() {
-    const [answerState, setAnswerState] = useState<AnswerState>(AnswerState.Unanswered)
     const [userAnswers, setUserAnswers] = useState<(string | null)[]>([])
 
-    const activeQuestionIndex = answerState === AnswerState.Unanswered ? userAnswers.length : userAnswers.length - 1;
+    const activeQuestionIndex = userAnswers.length;
 
     const quizIsOver = activeQuestionIndex === QUESTIONS.length;
 
     const handleSelectAnswer = useCallback((selectedAnswer: string | null) => {
-        setAnswerState(AnswerState.Answered);
-        setUserAnswers((prevUserAnswers) => [...prevUserAnswers, selectedAnswer]);
-        
-        setTimeout(() => {
-            if (selectedAnswer === QUESTIONS[activeQuestionIndex].answers[0]) {
-                setAnswerState(AnswerState.Correct);
-            } else {
-                setAnswerState(AnswerState.Wrong);
-            }
-            setTimeout(() => {
-                setAnswerState(AnswerState.Unanswered);
-            }, 2000);
-        }, 1000);
-    }, [activeQuestionIndex]);
+        setUserAnswers((prevUserAnswers) => {
+            return [...prevUserAnswers, selectedAnswer];
+        });
+    }, []);
     
     const handleSkipAnswer = useCallback(() => handleSelectAnswer(null), [handleSelectAnswer]);
     
@@ -48,19 +29,11 @@ export default function Quiz() {
 
     return (
         <div id="quiz">
-            <div id='question'>
-                <QuestionTimer
-                    key={activeQuestionIndex}
-                    timeout={15000}
-                    onTimeout={handleSkipAnswer}
-                    answerState={answerState} />
-                <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-                <Answers
-                    answers={QUESTIONS[activeQuestionIndex].answers}
-                    selectedAnswer={userAnswers[userAnswers.length - 1]}
-                    answerState={answerState}
-                    onSelect={handleSelectAnswer} />
-            </div>
+            <Question
+                key={activeQuestionIndex}
+                questionIndex={activeQuestionIndex}
+                onSelectAnswer={handleSelectAnswer}
+                onSkipAnswer={handleSkipAnswer} />    
         </div>
     )
 }
