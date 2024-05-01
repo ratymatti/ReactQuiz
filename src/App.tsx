1fb90fa0df3401289434e279861b50769c0e19df
Matti Räty <mattiraty@hotmail.com>
2024-05-01 13:21:20 +0300

    added components to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
+
     return (
-        <div>
-            Hello
-        </div>
+        <>
+            <Header />
+            <main>
+            <Quiz />
+            </main>
+        </>
     );
 }
 
